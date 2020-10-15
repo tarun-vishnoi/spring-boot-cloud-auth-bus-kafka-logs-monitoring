6c82cafbc239ca2b53c268db2c3b23265840c370
tarun-vishnoi <tarun.vce@gmail.com>
2020-10-15 15:28:36 +0530

    Dev Done- before Docker

--- config-server/src/main/java/com/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/config/ConfigServerApplication.java
@@ -1,5 +1,7 @@
 package com.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -8,7 +10,13 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigServerApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(ConfigServerApplication.class);
 	public static void main(String[] args) {
+		logger.info("info");
+		logger.debug("debug");
+		logger.error("error");
+		logger.warn("warn");
+		logger.trace("trace");
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
